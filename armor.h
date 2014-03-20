9c1bbf08d72749afa37e3e5cefccc2152e4545f1
Brian <bgreeneaka@gmail.com>
2014-03-20 18:54:00 +0000

    newItems

---
+++ armor.h
@@ -0,0 +1,10 @@
+#ifndef ARMOR_H
+#define ARMOR_H
+
+class Armor : public Item
+{
+public:
+    Armor();
+};
+
+#endif // ARMOR_H
---
+++ potion.h
@@ -0,0 +1,10 @@
+#ifndef POTION_H
+#define POTION_H
+
+class Potion : public Item
+{
+public:
+    Potion();
+};
+
+#endif // POTION_H
